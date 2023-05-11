875576f2b0260012f9e79b025f19f90f1e339230
JHeaon <j3heawon@gmail.com>
2023-05-11 20:14:26 +0900

    Fix "Modfiy mac_dev.sh"

--- sh_files/mac_dev.sh
+++ sh_files/mac_dev.sh
@@ -1,4 +1,4 @@
 pip3 install -r requirements.txt
-python backend/manage.py makemigrations
-python backend/manage.py migrate
-python backend/manage.py runserver
+python3 backend/manage.py makemigrations
+python3 backend/manage.py migrate
+python3 backend/manage.py runserver
